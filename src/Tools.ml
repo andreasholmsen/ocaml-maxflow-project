4b5e8ca6733d381e9e7227d8eec3bc47c896b24a
andreasholmsen <logins@andreash.no>
2025-11-17 11:26:51 +0100

    Init

---
+++ src/Tools.ml
@@ -0,0 +1,12 @@
+(* Yes, we have to repeat open Graph. *)
+open Graph
+
+(* assert false is of type ∀α.α, so the type-checker is happy. *) (* TODO: MAKE FUNCTION*)
+let clone_nodes (gr: 'a graph) = assert false
+;;
+
+
+
+
+let gmap _gr _f = assert false
+(* Replace _gr and _f by gr and f when you start writing the real function. *)
